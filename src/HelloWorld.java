2e2ec6137016ff7ac58c5e1cd5ab48aa62ab78e6
Domenik Radeck <domenik.radeck@tum.de>
2017-11-08 13:52:18 +0100

    Hello User is added.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner; 
+		
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+
+		System.out.println("Please enter your name: ");
+		Scanner scan = new Scanner(System.in);
+		String userName = scan.next();
+		HelloUser.greetUser(userName);
+		
+		
 	}
 
 }
